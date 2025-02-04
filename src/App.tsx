82b762ba0c02b9e28d1f4ebeebf789a6aa1cc7e1
Anthony HOANG <anthony.hoang@snowpact.com>
2025-02-04 23:27:26 +0100

    feat(setup): eslint and prettier setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
-import './App.css'
-import "@radix-ui/themes/styles.css";
-import { Button, Flex, Text } from '@radix-ui/themes';
+import { Text, Button, Flex } from '@radix-ui/themes';
+import './App.css';
+import '@radix-ui/themes/styles.css';
 
 function App() {
-
   return (
     <>
       <Flex direction="column" gap="2">
         <Text>Hello from Radix Themes :</Text>
-        <Button>Let's go</Button>
-		  </Flex>
+        <Button>{`Let's go`}</Button>
+      </Flex>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
