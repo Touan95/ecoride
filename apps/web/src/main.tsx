e142e7c20b47b94a66449e852597d36dfdc8951f
Anthony HOANG <anthony.hoang@snowpact.com>
2025-02-05 19:50:15 +0100

    feat(setup): monorepo architecture

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Theme>
+    <Theme radius="large" accentColor="grass">
       <BrowserRouter>
         <App />
       </BrowserRouter>
