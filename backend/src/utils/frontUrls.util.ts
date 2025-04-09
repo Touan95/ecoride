09cb62974d48ca6eeb8f85885e8cf480b8b6524e
Anthony HOANG <anthony.hoang@snowpact.com>
2025-04-09 19:53:58 +0200

    feat(deploy): clean envs and deploy to heroku

--- backend/src/utils/frontUrls.util.ts
+++
@@ -1,9 +0,0 @@
-import config from '../loader/config';
-
-export function buildAskResetPasswordUrl(token: string): string {
-  return `${config.WEBAPP_BASE_URL}${config.WEBAPP_RESET_PASSWORD_PATH}?token=${token}`;
-}
-
-export function buildJoinOrganizationUrl(token: string): string {
-  return `${config.WEBAPP_BASE_URL}${config.WEBAPP_JOIN_ORGANIZATION_PATH}?token=${token}`;
-}
