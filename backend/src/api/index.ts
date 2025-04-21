69584ea23bb81f19c9ddb3de71916cf904921a45
Anthony HOANG <anthony.hoang@snowpact.com>
2025-04-21 16:21:29 +0200

    chore(routes): clean backen routes (#16)

--- backend/src/api/index.ts
+++ backend/src/api/index.ts
@@ -1,13 +1,10 @@
 import { Router } from 'express';
 import { staffRouter } from './staff';
-import { agentRouter } from './agent';
 import { publicRouter } from './public';
 import { userRouter } from './user';
 import { adminRouter } from './admin';
 
 export default Router()
-  .use('/agent', agentRouter)
-  .use('/admin', staffRouter)
   .use('/user', userRouter)
   .use('/staff', staffRouter)
   .use('/admin', adminRouter)
