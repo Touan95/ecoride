6c25e2623c4161f3f50e6006ca4062db1de25a9b
Anthony HOANG <anthony.hoang@snowpact.com>
2025-05-03 21:53:49 +0200

    feat(seo): add legal page

---
+++ frontend/src/app/mentions-legales/page.tsx
@@ -0,0 +1,17 @@
+import { defaultSeoValues } from '@/configs/siteSettings';
+import { generatePageMetadata } from '@/utils/seo';
+import { seoData, SEO_PAGES } from '@/utils/seoPages';
+import { Metadata } from 'next';
+import LegalPageClient from './client';
+
+export const metadata: Metadata = await generatePageMetadata({
+  templateTitle: seoData[SEO_PAGES.PUBLIC_LEGAL_MENTIONS].title,
+  description: seoData[SEO_PAGES.PUBLIC_LEGAL_MENTIONS].description,
+  defaultMeta: defaultSeoValues,
+  noIndex: true,
+  noFollow: true
+});
+
+export default function LegalPage() {
+  return <LegalPageClient />;
+}
