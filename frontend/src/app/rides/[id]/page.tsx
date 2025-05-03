e0f19238fe957fa469bca02366138068b78cad62
Anthony HOANG <anthony.hoang@snowpact.com>
2025-05-03 20:15:51 +0200

    feat(seo): remove dynamic seo

--- frontend/src/app/rides/[id]/page.tsx
+++ frontend/src/app/rides/[id]/page.tsx
@@ -1,26 +1,13 @@
-import { useGetRideDetails } from '@/api/hooks/useUserAPI';
 import { defaultSeoValues } from '@/configs/siteSettings';
 import { generatePageMetadata } from '@/utils/seo';
 import { seoData, SEO_PAGES } from '@/utils/seoPages';
-import dayjs from 'dayjs';
 import { Metadata } from 'next';
-import { useParams } from 'next/navigation';
 import RideDetailsPageClient from './client';
 
 export async function generateMetadata(): Promise<Metadata> {
-  const { id: rideId } = useParams<{ id: string }>();
-  const { data: ride } = useGetRideDetails(rideId);
-
-  const title = ride
-    ? `Détail du trajet ${ride.departureLocation.city} vers ${ride.arrivalLocation.city} le ${dayjs(ride.departureDate).format('DD/MM/YYYY')}`
-    : seoData[SEO_PAGES.PUBLIC_RIDE_DETAIL].title;
-  const description = ride
-    ? `Consultez toutes les informations du trajet ${ride.departureLocation.city} vers ${ride.arrivalLocation.city} du ${dayjs(ride.departureDate).format('DD/MM/YYYY')}`
-    : seoData[SEO_PAGES.PUBLIC_RIDE_DETAIL].description;
-
   const metadata = await generatePageMetadata({
-    templateTitle: title,
-    description: description,
+    templateTitle: seoData[SEO_PAGES.PUBLIC_RIDE_DETAIL].title,
+    description: seoData[SEO_PAGES.PUBLIC_RIDE_DETAIL].description,
     defaultMeta: defaultSeoValues
   });
 
--- frontend/src/app/staff/disputes/[id]/page.tsx
+++ frontend/src/app/staff/disputes/[id]/page.tsx
@@ -1,29 +1,13 @@
-import { useGetOneReview, useGetRideDetails, useGetOneUser } from '@/api/hooks/useUserAPI';
 import { defaultSeoValues } from '@/configs/siteSettings';
 import { generatePageMetadata } from '@/utils/seo';
 import { seoData, SEO_PAGES } from '@/utils/seoPages';
 import { Metadata } from 'next';
-import { useParams } from 'next/navigation';
 import DisputeDetailPageClient from './client';
 
 export async function generateMetadata(): Promise<Metadata> {
-  const { id: reviewId } = useParams<{ id: string }>();
-  const { data: review } = useGetOneReview(reviewId);
-  const { data: ride } = useGetRideDetails(review?.rideId);
-  const { data: passenger } = useGetOneUser(review?.userId);
-
-  const title =
-    ride && passenger
-      ? `Litige : ${ride.departureLocation.city} -> ${ride.arrivalLocation.city} par ${passenger?.username}`
-      : seoData[SEO_PAGES.STAFF_DISPUTE_DETAIL].title;
-  const description =
-    ride && passenger
-      ? `Litige laissé par ${passenger?.username} sur le trajet ${ride.departureLocation.city} - ${ride.arrivalLocation.city}`
-      : seoData[SEO_PAGES.STAFF_DISPUTE_DETAIL].description;
-
   const metadata = await generatePageMetadata({
-    templateTitle: title,
-    description: description,
+    templateTitle: seoData[SEO_PAGES.STAFF_DISPUTE_DETAIL].title,
+    description: seoData[SEO_PAGES.STAFF_DISPUTE_DETAIL].description,
     defaultMeta: defaultSeoValues,
     noIndex: true,
     noFollow: true
